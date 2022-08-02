a7081a052701946f17a079902a7aee2131edc16f
Adam P <adam.phasey@hotmail.com>
2022-08-02 23:49:08 +0100

    set strict mode to false

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   images: {
     domains: ['s.gravatar.com', 'https://avatars.githubusercontent.com/u/92696504?v=4', 'avatars.githubusercontent.com']
